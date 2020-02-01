09c5554a1fc61823fc4f6a85affa118519a32e2e
Lương Công Hiếu <luonghieu184@gmail.com>
2020-02-01 19:01:33 +0700

    update alert notification

--- js/main.js
+++ js/main.js
@@ -81,3 +81,50 @@ $("span#logout").click(function() {
   document.querySelector(".login.active").classList.remove("is-hidden");
   location.reload(true);
 })
+
+// The main class
+var AlertBox = function(id, option) {
+  this.show = function(msg) {
+      var alertArea = document.querySelector(id);
+      var alertBox = document.createElement('DIV');
+      var alertContent = document.createElement('DIV');
+      var alertClose = document.createElement('A');
+      var alertClass = this;
+      alertContent.classList.add('alert-content');
+      alertContent.innerText = msg;
+      alertClose.classList.add('alert-close');
+      alertClose.setAttribute('href', '#');
+      alertBox.classList.add('alert-box');
+      alertBox.appendChild(alertContent);
+      if (!option.hideCloseButton || typeof option.hideCloseButton === 'undefined') {
+        alertBox.appendChild(alertClose);
+      }
+      alertArea.appendChild(alertBox);
+      alertClose.addEventListener('click', function(event) {
+        event.preventDefault();
+        alertClass.hide(alertBox);
+      });
+      if (!option.persistent) {
+        var alertTimeout = setTimeout(function() {
+          alertClass.hide(alertBox);
+          clearTimeout(alertTimeout);
+        }, option.closeTime);
+      }
+  };
+
+  this.hide = function(alertBox) {
+    alertBox.classList.add('hide');
+    var disperseTimeout = setTimeout(function() {
+      alertBox.parentNode.removeChild(alertBox);
+      clearTimeout(disperseTimeout);
+    }, 500);
+  };
+};
+
+// Thiết lập thời gian để tắt alert
+
+const alertbox = new AlertBox('#alert-area', {
+  closeTime: 3000,
+  persistent: false,
+  hideCloseButton: false
+});
