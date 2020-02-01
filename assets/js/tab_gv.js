cd8ef2288d7c7d6feccd5f51e2066c552f2ecf5a
Lương Công Hiếu <luonghieu184@gmail.com>
2020-02-01 20:01:06 +0700

    fix get value

--- assets/js/tab_gv.js
+++ assets/js/tab_gv.js
@@ -4,24 +4,20 @@ $('nav a').click(function(e) {
   e.preventDefault();
   $('nav a').removeClass('active');
   $(this).addClass('active');
-  if(this.id === !'studentManager'){
+  if (this.id === !'studentManager') {
     $('.studentManager').addClass('noshow');
-  }
-  else if(this.id === 'studentManager') {
+  } else if (this.id === 'studentManager') {
     $('.studentManager').removeClass('noshow');
     $('.rightbox').children().not('.studentManager').addClass('noshow');
-    if($('.studentAll').html() == "" || $('.studentAll').hasClass('formtest') == undefined)
-    {
+    if ($('.studentAll').html() == "" || $('.studentAll').hasClass('formtest') == undefined) {
       var jsLoading = '<form class="form-view full-width"><div class="form-input box-loading">  <input class="text input input_f disabled" type="text">  </div><div class="form-input heigh-250px box-loading">  <input class="input input_f disabled" type="text">  </div> <div class="form-input box-loading">  <input class="input input_f disabled" type="text">  </div><div class="box-loading">  <input class="input input_f disabled box-loading" type="text">  </div><div class="box-loading">  <input class="input input_f" type="text">  </div><div class="box-loading">  <input class="input input_f" type="text">  </div><div class="form-input-absoluted box-loading"></div> </form>';
       $('.studentAll').append(jsLoading);
       studentManager_();
     }
-  }
-  else if (this.id === 'profile') {
+  } else if (this.id === 'profile') {
     $('.profile').removeClass('noshow');
     $('.rightbox').children().not('.profile').addClass('noshow');
-    if($('.profileGV').html() == "" || $('.profileGV').hasClass('formtest') == undefined)
-    {
+    if ($('.profileGV').html() == "" || $('.profileGV').hasClass('formtest') == undefined) {
       var jsLoading = '<form class="form-view"><div class="form-input validate-input box-loading"> <span class="label-input">Họ và tên:</span> <input class="text input input_f disabled" type="text"> <span class="focus-input"></span> </div><div class="form-input validate-input box-loading"> <span class="label-input">Email:</span> <input class="input input_f disabled" type="text"> <span class="focus-input"></span> </div><div class="form-input validate-input box-loading"> <span class="label-input">Số điện thoại:</span> <input class="input input_f disabled box-loading" type="text"> <span class="focus-input"></span> </div><div class="form-input validate-input box-loading"> <span class="label-input">Nhập mật khẩu cũ:</span> <input class="input input_f" type="text"> <span class="focus-input"></span> </div><div class="form-input validate-input box-loading" > <span class="label-input">Nhập mật khẩu mới:</span> <input class="input input_f" type="text"> <span class="focus-input"></span> </div><div class="form-input-absoluted box-loading"></div> </div></form>';
       $('.profileGV').append(jsLoading);
       profile_();
@@ -32,31 +28,25 @@ $('nav a').click(function(e) {
     //     alert('ĐÃ TẢI');
     //   }
     // }
-  }
-  else if(this.id === 'subscription') {
+  } else if (this.id === 'subscription') {
     $('.subscription').removeClass('noshow');
     $('.rightbox').children().not('.subscription').addClass('noshow');
-    if($('#view_topic_st_reg').html() == "")
-    {
+    if ($('#view_topic_st_reg').html() == "") {
       var jsLoading = '<form class="form-view full-width"><div class="form-input box-loading">  <input class="text input input_f disabled" type="text">  </div><div class="form-input heigh-250px box-loading">  <input class="input input_f disabled" type="text">  </div> <div class="form-input box-loading">  <input class="input input_f disabled" type="text">  </div><div class="box-loading">  <input class="input input_f disabled box-loading" type="text">  </div><div class="box-loading">  <input class="input input_f" type="text">  </div><div class="box-loading">  <input class="input input_f" type="text">  </div><div class="form-input-absoluted box-loading"></div> </form>';
       $('#view_topic_st_reg').append(jsLoading);
       topicManager_();
     }
 
-  }
-    else if(this.id === 'privacy') {
+  } else if (this.id === 'privacy') {
     $('.privacy').removeClass('noshow');
     $('.rightbox').children().not('.privacy').addClass('noshow');
-  }
-  else if(this.id === 'settings') {
+  } else if (this.id === 'settings') {
     $('.settings').removeClass('noshow');
     $('.rightbox').children().not('.settings').addClass('noshow');
-  }
-  else if(this.id === 'pushNotification') {
+  } else if (this.id === 'pushNotification') {
     $('.pushNotification').removeClass('noshow');
     $('.rightbox').children().not('.pushNotification').addClass('noshow');
-  }
-  else if(this.id === 'notif') {
+  } else if (this.id === 'notif') {
     $('.notif').removeClass('noshow');
     $('.rightbox').children().not('.notif').addClass('noshow');
   }
@@ -92,55 +82,56 @@ $(document).ready(function() {
   }, 5000);
 });
 
-function viewrp(mssv){
-    $(".modelDetails").css("display","block")
+function viewrp(mssv) {
+  $(".modelDetails").css("display", "block")
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.modelDetails-close').click(function(event) {
-      $('.modelDetails').css("display","none");
-      $('.modelCT_Decription').css("display","none");
+    $('.modelDetails').css("display", "none");
+    $('.modelCT_Decription').css("display", "none");
   });
 });
 
 function studentManager_() {
-  if(getCoo56yhjkk() == undefined)
-  return;
-  var url = APIurl  + getCoo56yhjkk() + "&action=ltLookup";
+  if (getCoo56yhjkk() == undefined)
+    return;
+  var url = APIurl + getCoo56yhjkk() + "&action=ltLookup";
 
   $.getJSON(url, function(data) {
     var strText = '<div class="divTable"> <div class="divHeading"> <div class="cell_ith"> MSSV </div> <div class="cell_ith">Họ và tên</div> <div class="cell_ith"> Email </div> <div class="cell_ith"> Số điện thoại </div> <div class="cell_ith"> Ngày sinh </div> <div class="cell_ith"> Mã lớp </div> <div class="cell_ith"> Tên môn học </div> <div class="cell_ith"> Báo Cáo </div> </div>';
     $.each(data.lookup, function(key, val) {
-      strText += '<div class="row_ith">'+ '<div class="cell_ith">'+ val["sv-masv"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-hoten"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-email"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-sdt"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-birth"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-malop"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-tenmonhoc"]  + '</div>';
-      strText += '<div class="cell_ith">'+ '<button class="btn-hutech more" onclick="viewrp('+val["sv-masv"]+')"><i class="fas fa-search"><span>&nbsp;REVIEW</span></i></button>'  + "</div></div>";
+      strText += '<div class="row_ith">' + '<div class="cell_ith">' + val["sv-masv"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-hoten"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-email"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-sdt"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-birth"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-malop"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-tenmonhoc"] + '</div>';
+      strText += '<div class="cell_ith">' + '<button class="btn-hutech more" onclick="viewrp(' + val["sv-masv"] + ')"><i class="fas fa-search"><span>&nbsp;REVIEW</span></i></button>' + "</div></div>";
     });
     strText += "</div>";
-      $(".studentAll").html(strText);
-      $("span#total_stduent").html(data.extension.tongDSHD);
+    $(".studentAll").html(strText);
+    $("span#total_stduent").html(data.extension.tongDSHD);
   });
 }
 $(".studentManager").ready(function() {
-studentManager_();
+  studentManager_();
 })
-function profile_(){
-  if(getCoo56yhjkk() == undefined)
-  return;
-  var url = APIurl  + getCoo56yhjkk() + "&action=ltprofile";
+
+function profile_() {
+  if (getCoo56yhjkk() == undefined)
+    return;
+  var url = APIurl + getCoo56yhjkk() + "&action=ltprofile";
 
   $.getJSON(url, function(data) {
     var strText = '<div class="formtest">';
     $.each(data, function(key, val) {
       strText += '<div class="title"> <h2>thông tin cá nhân giảng viên</h2> </div>';
       strText += '<form class="form-view" onsubmit="return false;">'
-      strText += '<div class="form-input validate-input" data-validate="Vui lòng nhập họ tên"> <span class="label-input">Họ và tên:</span> <input class="input input_f disabled" type="text" value="'+val[0]["gv-hoten"]+'" placeholder="Nhập họ và tên"> <span class="focus-input"></span> </div>';
-      strText += '<div class="form-input validate-input" data-validate="Nhập đúng định dạng email"> <span class="label-input">Email:</span> <input class="input input_f disabled" type="text" value="'+ val[0]["gv-email"]+'" placeholder="Nhập email"> <span class="focus-input"></span> </div>'  ;
-      strText += '<div class="form-input validate-input" data-validate="Số điện thoại là bắt buộc"> <span class="label-input">Số điện thoại:</span> <input class="input input_f disabled" type="text" value="'+ val[0]["gv-phone"]+'" placeholder="Nhập số điện thoại"> <span class="focus-input"></span> </div></form>';
+      strText += '<div class="form-input validate-input" data-validate="Vui lòng nhập họ tên"> <span class="label-input">Họ và tên:</span> <input class="input input_f disabled" type="text" value="' + val[0]["gv-hoten"] + '" placeholder="Nhập họ và tên"> <span class="focus-input"></span> </div>';
+      strText += '<div class="form-input validate-input" data-validate="Nhập đúng định dạng email"> <span class="label-input">Email:</span> <input class="input input_f disabled" type="text" value="' + val[0]["gv-email"] + '" placeholder="Nhập email"> <span class="focus-input"></span> </div>';
+      strText += '<div class="form-input validate-input" data-validate="Số điện thoại là bắt buộc"> <span class="label-input">Số điện thoại:</span> <input class="input input_f disabled" type="text" value="' + val[0]["gv-phone"] + '" placeholder="Nhập số điện thoại"> <span class="focus-input"></span> </div></form>';
 
       // Change password form
       strText += '<form class="form-view top-10px" onsubmit="return false;">';
@@ -148,7 +139,7 @@ function profile_(){
 
     });
     strText += '</form></div>';
-      $(".profileGV").html(strText);
+    $(".profileGV").html(strText);
 
   });
 }
@@ -156,28 +147,28 @@ $(".profile").ready(function() {
   profile_();
 })
 
-function topicManager_(){
-  if(getCoo56yhjkk() == undefined)
-  return;
-  var url = APIurl  + getCoo56yhjkk() + "&action=lt_view_topic";
+function topicManager_() {
+  if (getCoo56yhjkk() == undefined)
+    return;
+  var url = APIurl + getCoo56yhjkk() + "&action=lt_view_topic";
 
   $.getJSON(url, function(data) {
     var strText = '<div class="divTable"> <div class="divHeading"> <div class="cell_ith">Thời gian đăng ký</div> <div class="cell_ith">MSSV </div> <div class="cell_ith">Họ và tên</div> <div class="cell_ith">Tên đề tài</div> <div class="cell_ith">Mô tả đề tài</div> <div class="cell_ith">Tên môn học </div><div class="cell_ith">Công cụ </div></div>';
     $.each(data.lookup, function(key, val) {
-      strText += '<div class="row_ith">'+ '<div class="cell_ith">'+ val["time-reg"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-masv"] + '</div>';
-      strText += '<div class="cell_ith">' +val["sv-hoten"] + '</div>';
+      strText += '<div class="row_ith">' + '<div class="cell_ith">' + val["time-reg"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-masv"] + '</div>';
+      strText += '<div class="cell_ith">' + val["sv-hoten"] + '</div>';
 
-      strText += '<div class="cell_ith">' +val["ten-detai"] + '</div>';
-      strText += '<div class="cell_ith" style="width: 300px">' +val["mota-detai"] + '</div>';
-      strText += '<div class="cell_ith">' +val["ten-monhoc"] + '</div>';
+      strText += '<div class="cell_ith">' + val["ten-detai"] + '</div>';
+      strText += '<div class="cell_ith" style="width: 300px">' + val["mota-detai"] + '</div>';
+      strText += '<div class="cell_ith">' + val["ten-monhoc"] + '</div>';
 
       strText += '<div class="cell_ith">' + '<button class="btn-hutech more" onclick="xemmotadetai(' + val["sv-masv"] + ')">XEM ĐỀ TÀI VÀ KIỂM DUYỆT</button>' + "</div></div>";
       strText += '</div>';
 
     });
     strText += "</div>";
-      $("#view_topic_st_reg").html(strText);
+    $("#view_topic_st_reg").html(strText);
 
   });
 }
@@ -190,8 +181,8 @@ $("#pushsubmit").click(function(event) {
   var title = $("#pushtitlecontent").val();
   if (content.length == "" || content.length <= 10)
     return alert('Nội dung quá ngắn hoặc bạn chưa nhập nội dung! Xin cảm ơn');
-  if (title.length == "" || title.length >50)
-      return alert('Tiêu đề không được bỏ trống và không quá 50 ký tự! Xin cảm ơn');
+  if (title.length == "" || title.length > 50)
+    return alert('Tiêu đề không được bỏ trống và không quá 50 ký tự! Xin cảm ơn');
   document.querySelector(".form-input-absoluted .downloading").classList.remove("is-hidden");
   $.ajax({
     url: "https://script.google.com/macros/s/AKfycbzYPTjObWfAe0sBQrCCjPN1FjYxdJ1Vp178WIN5rrnUeRlzw4ft/exec?request=ltpush&" + getCoo56yhjkk(),
@@ -221,8 +212,8 @@ $("#sendconfirm").click(function(event) {
   var title = $("#pushtitlecontent").val();
   if (content.length == "" || content.length <= 10)
     return alert('Nội dung quá ngắn hoặc bạn chưa nhập nội dung! Xin cảm ơn');
-  if (title.length == "" || title.length >50)
-      return alert('Tiêu đề không được bỏ trống và không quá 50 ký tự! Xin cảm ơn');
+  if (title.length == "" || title.length > 50)
+    return alert('Tiêu đề không được bỏ trống và không quá 50 ký tự! Xin cảm ơn');
   document.querySelector(".form-input-absoluted .sending").classList.remove("is-hidden");
   $.ajax({
     url: "https://script.google.com/macros/s/AKfycbzYPTjObWfAe0sBQrCCjPN1FjYxdJ1Vp178WIN5rrnUeRlzw4ft/exec?request=ltsendEmail&" + getCoo56yhjkk(),
@@ -240,12 +231,12 @@ $("#sendconfirm").click(function(event) {
   });
 })
 $("#sendEmail").click(function(event) {
-    $(".modelDetails").css("display","block")[0];
+  $(".modelDetails").css("display", "block")[0];
 })
 $('.modelDetails').ready(function() {
   var span = document.getElementsByClassName('cancelconfirm')[0];
   span.onclick = function() {
-    $('.modelDetails').css("display","none");
+    $('.modelDetails').css("display", "none");
   }
 });
 
@@ -254,8 +245,8 @@ function xemmotadetai(mssv) {
   $(".modelCT_Decription").css("display", "block")[0];
   $('.wrapper-content-description').ready(function() {
 
-  $("#sv_content_description").html(jsLD);
-    var url = APIurl +"&mssv=" + mssv + "&action=xemmotadetai";
+    $("#sv_content_description").html(jsLD);
+    var url = APIurl + "&mssv=" + mssv + "&action=xemmotadetai";
     $.getJSON(url, function(data) {
       var editor = tui.Editor.factory({
         el: document.querySelector('#sv_content_description'),
@@ -268,20 +259,22 @@ function xemmotadetai(mssv) {
   });
 }
 $(".modelCT_Decription").ready(function() {
-  var note = $("#sendtext_optionalEmail").val();
-  var mssv = $.trim($("#mssv_require").val()).replace(/ /g, '');
   $("#approved_detai").click(function(event) {
-      var url = APIurl + getCoo56yhjkk() +"&mssv=" + mssv + "&note=" + note +  "&action=duyetdetai";
-      $.getJSON(url, function(data) {
-        // return alert(data.result)
-        alertbox.show(data.result);
-      });
+    var note = $("#sendtext_optionalEmail").val();
+    var mssv = $.trim($("#mssv_require").val()).replace(/ /g, '');
+    var url = APIurl + getCoo56yhjkk() + "&mssv=" + mssv + "&note=" + note + "&action=duyetdetai";
+    $.getJSON(url, function(data) {
+      // return alert(data.result)
+      alertbox.show(data.result);
+    });
   })
   $("#reject_detai").click(function(event) {
-      var url = APIurl + getCoo56yhjkk() +"&mssv=" + mssv + "&note=" + note +  "&action=tuchoidetai";
-      $.getJSON(url, function(data) {
-        // return alert(data.result)
-        alertbox.show(data.result);
-      });
+    var note = $("#sendtext_optionalEmail").val();
+    var mssv = $.trim($("#mssv_require").val()).replace(/ /g, '');
+    var url = APIurl + getCoo56yhjkk() + "&mssv=" + mssv + "&note=" + note + "&action=tuchoidetai";
+    $.getJSON(url, function(data) {
+      // return alert(data.result)
+      alertbox.show(data.result);
+    });
   })
 });
